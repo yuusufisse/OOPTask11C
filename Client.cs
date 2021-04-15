18f175c83cbb21bff6fa764abe5b9f9d83f44683
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-04-15 19:59:05 +0300

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPTask11C
+{
+    class Client
+    {
+        public Client(string name, DateTime dateOfRegistration)
+        {
+            Name = name;
+            DateOfRegistration = dateOfRegistration;
+        }
+
+        public string Name { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPTask11C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Public\Json\client.json";
+            List<Client> clients = new List<Client>();
+            using(StreamReader streamReader=new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                //Deserialization...
+                clients = JsonConvert.DeserializeObject<List<Client>>(jsonString);
+            }
+            foreach (var c in clients)
+                Console.WriteLine("Name: " + c.Name + ", Date of registration: " + c.DateOfRegistration.ToShortDateString());
+        }
+    }
+}
